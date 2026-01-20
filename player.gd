dc7329eaac5a084e1b30ce4d6aaef0eae79d0ea4
Raul Dias <raulhendrix@gmail.com>
2026-01-19 23:00:37 -0300

    first moves with stakeholders (assets)

---
+++ player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+var direction: Vector2
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+func _process(delta: float) -> void:
+	direction=Vector2.ZERO
+	if(Input.is_action_pressed("ui_up")):
+		direction.y += -1
+	if(Input.is_action_pressed("ui_down")):
+		direction.y += 1
+	if(Input.is_action_pressed("ui_left")):
+		direction.x += -1
+		
+	if(Input.is_action_pressed("ui_right")):
+		direction.x += 1
+		
+	direction=direction.normalized()
+	
+
+func _physics_process(delta: float) -> void:
+	
+	velocity=direction  * SPEED
+
+	move_and_slide()
